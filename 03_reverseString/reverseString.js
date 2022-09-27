60e0c5ae300e652dc6332ee4859d134ddb5464b8
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-27 16:07:28 -0400

    solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const chars = str.split('');
+  let len = chars.length;
+  let res = "";
+  for (let i = 1; i <= len; i++)
+    res += chars[len - i];
+  return res;
 };
+
+/* Learned:
+    JS indexes are positive integers. 
+    Negative indeces return undefined values
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
