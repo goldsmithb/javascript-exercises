adb441f5a138203305f31025813f7aeb44e0c3d1
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-27 14:20:05 -0400

    solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let res = "";
+  for (let i = 0; i < num; i++) {
+    res += str;
+  }
+  return res;
 };
 
 // Do not edit below this line
