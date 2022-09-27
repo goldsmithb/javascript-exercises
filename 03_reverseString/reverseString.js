2e519cd01d51aec4c0b2681b4c0e9cfb06ec015f
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-27 16:36:35 -0400

    03 sol written

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,17 +1,24 @@
-const reverseString = function(str) {
-  const chars = str.split('');
-  let len = chars.length;
-  let res = "";
-  for (let i = 1; i <= len; i++)
-    res += chars[len - i];
-  return res;
+const removeFromArray = function(arr, ...delItems) {
+  // for each item to delete,
+  for (const item in delItems) {
+    // traverse the entire array and
+    for (let i = 0; i < arr.length; i++) {
+      // remove all instances of the item
+      if (arr[i] == item) arr.splice(i, 1);
+    }
+  }
 };
 
-/* Learned:
-    JS indexes are positive integers. 
-    Negative indeces return undefined values
-
-*/
+/*
+ * What I learned:
+ * Rest parameters:
+    * A function definition's last parameter can be prefixed with ... 
+    * (three U+002E FULL STOP characters), which will cause all remaining 
+    * (user supplied) parameters to be placed within a standard JavaScript 
+    * array. Only the last parameter in a function definition can be a rest 
+    * parameter.
+  * Using splice to remove array elements without leaving holes
+ * */
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = removeFromArray;
