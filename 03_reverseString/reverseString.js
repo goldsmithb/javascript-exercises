3f4fc11aa1fc4e5c955646c0a522f37714a778a2
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-27 21:01:08 -0400

    fixed function mix up

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,24 +1,6 @@
-const removeFromArray = function(arr, ...delItems) {
-  // for each item to delete,
-  for (const item in delItems) {
-    // traverse the entire array and
-    for (let i = 0; i < arr.length; i++) {
-      // remove all instances of the item
-      if (arr[i] == item) arr.splice(i, 1);
-    }
-  }
+const reverseString = function() {
+  return "HI";
 };
 
-/*
- * What I learned:
- * Rest parameters:
-    * A function definition's last parameter can be prefixed with ... 
-    * (three U+002E FULL STOP characters), which will cause all remaining 
-    * (user supplied) parameters to be placed within a standard JavaScript 
-    * array. Only the last parameter in a function definition can be a rest 
-    * parameter.
-  * Using splice to remove array elements without leaving holes
- * */
-
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...delItems) {
+  // for each item to delete,
+  for (const item in delItems) {
+    // traverse the entire array and
+    for (let i = 0; i < arr.length; i++) {
+      // remove all instances of the item
+      if (arr[i] == item) arr.splice(i, 1);
+    }
+  
 };
+
+/*
+ * What I learned:
+ * Rest parameters:
+    * A function definition's last parameter can be prefixed with ... 
+    * (three U+002E FULL STOP characters), which will cause all remaining 
+    * (user supplied) parameters to be placed within a standard JavaScript 
+    * array. Only the last parameter in a function definition can be a rest 
+    * parameter.
+  * Using splice to remove array elements without leaving holes
+ * */
 
 // Do not edit below this line
 module.exports = removeFromArray;
