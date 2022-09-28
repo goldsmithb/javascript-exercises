8c34b1364c8f20f195d0001e5d0d8ecb0a148353
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-28 13:41:08 -0400

    05 fixed larger number first bug

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,11 @@
 /* return the sum of all integers from s to e */
-const sumAll = function(s, e) {
+const sumAll = function(a, b) {
+  // set start and end for range
+  let start, end;
+  if (b > a) start = a, end = b;
+  else start = b, end = a;
   let sum = 0;
-  for (let x = s; x <= e; x++) sum += x;
+  for (let x = start; x <= end; x++) sum += x;
   return sum;
 };
 
