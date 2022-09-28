74c38b3a60fe6b0d113c82f7f32b91539ab92fdc
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-27 21:06:15 -0400

    sol 03 restored. sol 04 restored

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-  return "HI";
-};
+const reverseString = function(str) {
+  const chars = str.split('');
+  let len = chars.length;
+  let res = "";
+  for (let i = 1; i <= len; i++)
+    res += chars[len - i];
+  return res;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
