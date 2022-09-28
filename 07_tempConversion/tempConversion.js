677f5849ff949b0a5488e5e183e8861a19e6d2da
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-28 14:09:41 -0400

    07 rounding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+// c = f*(9/5) + 32
+// f = (x - 32) * (5/9)
 
+const DECIMAL_PLACE = 10;
+
+const ftoc = function(temp) {
+  temp = (temp - 32) * (5/9);
+  temp = Math.round(temp * DECIMAL_PLACE) / DECIMAL_PLACE;
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * (9/5) + 32;
+  temp = Math.round(temp * DECIMAL_PLACE) / DECIMAL_PLACE;
+  return temp;
 };
 
 // Do not edit below this line
