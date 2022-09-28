e214170c9e093a9b41c87275b436b87c651fa827
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-28 13:27:42 -0400

    05 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+/* return the sum of all integers from s to e */
+const sumAll = function(s, e) {
+  let sum = 0;
+  for (let x = s; x <= e; x++) sum += x;
+  return sum;
 };
 
 // Do not edit below this line
