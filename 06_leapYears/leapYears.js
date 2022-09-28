360c3d33b381cb79f3e257a67fa231ace2174f0b
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-28 13:59:31 -0400

    06 solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -10,9 +10,8 @@ Leap years are determined by the following rules:
   pretty confusing) */
 const leapYears = function(year) {
   if (year % 4 === 0) {
-    if (year % 100 === 0) {
+    if (year % 100 === 0)
       return year % 400 === 0 ? true : false;
-    }
     else return true;
   }
   return false;
