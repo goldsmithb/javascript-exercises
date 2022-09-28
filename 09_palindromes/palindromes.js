e16b6855f6ae420b604bc3f66ee306bc14501789
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-28 14:38:52 -0400

    09 proper use of toLowerCase() method

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+// determine if a string is a palindrome
+// Recursive solution
+const palindromes = function (str) {
+  // clean input - normalize caps, remove any punctuation
+  console.log(str);
+  str = str.toLowerCase();
+  // from: https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
+  str = str.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
+  console.log(str);
 
+  // Base cases
+  if (str.length === 2) {
+    return (str[0] === str[1]) ? true : false;
+  }
+  else if (str.length === 3) {
+    return (str[0] === str[2]) ? true : false;
+  }
+  // recursion
+  if (str[0] !== str[str.length - 1]) 
+  {
+    return false;
+  }
+  else {
+    return palindromes(str.slice(1, -1));
+  }
 };
 
 // Do not edit below this line
