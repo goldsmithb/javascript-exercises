1916c6c1b27c5c78a1b0ef211bc2ae7884f83d68
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-28 13:54:43 -0400

    06 fixed logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -9,8 +9,14 @@ Leap years are determined by the following rules:
   were in fact leap years). (Yes, it's all 
   pretty confusing) */
 const leapYears = function(year) {
-  if (year % 4 === 0 && year % 100 !== 0) return true;
-  else return false;
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) return true;
+      else return false;
+    }
+    else return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
