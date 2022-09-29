f296cf267acc25e8b2a1dbe8a7cf979e5a3499fc
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-09-29 12:57:07 -0400

    setup 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,17 @@
-const caesar = function() {
+/*
+'ABC'.charCodeAt(0)  // returns 65
+'ZYX'.charCodeAt(0)  // returns 90
+'abc'.charCodeAt(0)  // returns 97
+'zyx'.charCodeAt(0)  // returns 122
+*/
+/* write a function that takes a string to be encoded 
+ * and a shift factor and then returns the encoded string
+ * e.g. : caesar('A', 1) // simply shifts the letter by 1: 
+ *        returns 'B'
 
+*/
+const caesar = function(message, shift) {
+  
 };
 
 // Do not edit below this line
